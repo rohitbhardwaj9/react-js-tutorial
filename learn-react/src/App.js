9825b9b757af56f81e9e9765130933977752086f
Rohit Bhardwaj <78081974+rohitbhardwaj9@users.noreply.github.com>
2023-08-05 13:00:36 +0000

    onClick event without State

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -3,16 +3,23 @@ import './App.css';
 import User from './User';
 
 function App() {
+
+  let data = "Rohit";
+  function updateData(){
+    data = "Bhardwaj";
+    alert(data);
+  }
+
   return (
-    // start
-    // This is JSX code
-    //JSX stands for Javascript XML
-    //JSX allow us to write HTML in react using javascript file
+    //here we are expecting to change data value from Rohit to Bhardwaj on button click
+    //but this will not happen
+    //just because of, we are using data variable
+    //And React does not provide this feature to us, to achieve the epected output we have to use STATE 
     <div className="App">
-      <h1>Hello World !</h1>
+      <h1>{data}</h1>
+      <button onClick={updateData}>Click me!</button>
       <User/>
     </div>
-    //end
   );
 }
 
--- learn-react/src/User.js
+++ learn-react/src/User.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 class User extends Component {
     render(){
-        return React.createElement("div", null, React.createElement("h1", null, "Yo, This is without JSX!"))
+        return React.createElement("div", null, React.createElement("h1", null, "Click Event and Function Example"))
     }
 }
 
