20b925e8e96485d2f125d51d0c8000aeefa5b888
Rohit Bhardwaj <78081974+rohitbhardwaj9@users.noreply.github.com>
2023-08-05 13:06:04 +0000

    onClick Update : State

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,20 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import User from './User';
+import { useState } from 'react';
 
 function App() {
 
-  let data = "Rohit";
+  const [data, setData] = useState("Rohit");
+
   function updateData(){
-    data = "Bhardwaj";
-    alert(data);
+    setData("Bhardwaj");
   }
+
 
   return (
     //here we are expecting to change data value from Rohit to Bhardwaj on button click
-    //but this will not happen
-    //just because of, we are using data variable
-    //And React does not provide this feature to us, to achieve the epected output we have to use STATE 
+    
     <div className="App">
       <h1>{data}</h1>
       <button onClick={updateData}>Click me!</button>
