8bac74f9926542efcca50af7592c0dd7e68d3b55
Rohit Bhardwaj <78081974+rohitbhardwaj9@users.noreply.github.com>
2023-08-05 07:54:04 +0000

    Class component : by importing react and using export default

---
+++ learn-react/src/User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class User extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello User from class component!</h1>
+            </div>
+        );
+    }
+}
+
+export default User;
